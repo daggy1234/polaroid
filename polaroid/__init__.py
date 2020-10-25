022fb1366609185ff4db9ca1821bfd0f2287b1e7
Arnav Jindal <arnav.jindal7@gamil.com>
2020-10-25 12:51:52 +0530

    Moved to Travis

--- polaroid/__init__.py
+++ polaroid/__init__.py
@@ -11,7 +11,7 @@ Hyper Fast and safe image manipulation library for python . Powered by rust.
 from .polaroid import Image,Gif
 from collections import namedtuple
 
-__version__ = '0.2.1'
+__version__ = '0.2.3'
 __author__ = "Daggy1234"
 __license__ = "MIT"
 __copyright__ = "Copyright 2020 Daggy1234"
