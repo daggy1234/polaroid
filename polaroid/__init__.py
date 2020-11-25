b3c12d34fb7c80c0d78b203e8cf0528a78fc830a
Arnav Jindal <arnav.jindal7@gamil.com>
2020-11-26 02:50:35 +0530

    Fixed Wheels

--- polaroid/__init__.py
+++ polaroid/__init__.py
@@ -10,7 +10,7 @@ Hyper Fast and safe image manipulation library for python . Powered by rust.
 from .polaroid import Image,Gif, Rgb
 from collections import namedtuple
 
-__version__ = '0.2.9'
+__version__ = '0.2.10'
 __author__ = "Daggy1234"
 __license__ = "MIT"
 __copyright__ = "Copyright 2020 Daggy1234"
@@ -20,5 +20,5 @@ __all__ = ["Image","Gif","Rgb"]
 VersionInfo = namedtuple('VersionInfo',
                          'major minor micro releaselevel serial')
 
-version_info = VersionInfo(major=0, minor=2, micro=9, releaselevel='development',
+version_info = VersionInfo(major=0, minor=2, micro=10, releaselevel='development',
                            serial=0)
