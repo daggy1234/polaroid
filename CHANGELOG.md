eaf46d46b3e701da9a91b0938dd9f4d60e0f7f08
Arnav <arnav.jindal7@gmail.com>
2021-03-03 09:49:44 +0530

    verioning

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Changelog
+
+Sorry but due to my own poor memory there is no changelog. View changelog from v0.3.0.
+
+### [0.3.0](https://github.com/Daggy1234/polaroid/releases/tag/v0.3.0) (2021-3-03)
+
+- Seam Carving (Liquid rescale)
+- color stuff (colorize, gradient)
+- Hog
+- Better Gif supprt [Still unuseable]
+- Save image to jpeg (better compression)
+- New magic methods
--- polaroid/__init__.py
+++ polaroid/__init__.py
@@ -10,7 +10,7 @@ Hyper Fast and safe image manipulation library for python . Powered by rust.
 from .polaroid import Image, Rgb, Gif, ImageDraw, Rgba
 from collections import namedtuple
 
-__version__ = '0.2.10'
+__version__ = '0.3.0'
 __author__ = "Daggy1234"
 __license__ = "MIT"
 __copyright__ = "Copyright 2020 Daggy1234"
@@ -20,5 +20,5 @@ __all__ = ["Image", "Rgb", "Gif", "ImageDraw"]
 VersionInfo = namedtuple('VersionInfo',
                          'major minor micro releaselevel serial')
 
-version_info = VersionInfo(major=0, minor=2, micro=10, releaselevel='development',
+version_info = VersionInfo(major=0, minor=3, micro=0, releaselevel='development',
                            serial=0)
