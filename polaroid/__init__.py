b5e3015f77846f6be1479298beb8301342ba8b5e
Arnav <arnav.jindal7@gmail.com>
2021-03-09 23:07:40 +0530

    Annoying Prints

--- polaroid/__init__.py
+++ polaroid/__init__.py
@@ -10,7 +10,7 @@ Hyper Fast and safe image manipulation library for python . Powered by rust.
 from .polaroid import Image, Rgb, Gif, Rgba
 from collections import namedtuple
 
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 __author__ = "Daggy1234"
 __license__ = "MIT"
 __copyright__ = "Copyright 2021 Daggy1234"
@@ -20,5 +20,5 @@ __all__ = ["Image", "Rgb", "Gif", "Rgba"]
 VersionInfo = namedtuple('VersionInfo',
                          'major minor micro releaselevel serial')
 
-version_info = VersionInfo(major=0, minor=3, micro=1, releaselevel='development',
+version_info = VersionInfo(major=0, minor=3, micro=2, releaselevel='development',
                            serial=0)
